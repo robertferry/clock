5981981a438a4e098ffbf425ca948791148ca0b6
robertferry <robertmferry@gmail.com>
2022-11-17 09:26:21 -0600

    clock works

---
+++ script.js
@@ -0,0 +1,25 @@
+const hour = document.querySelector('.hour-hand');
+const min = document.querySelector('.min-hand');
+const sec = document.querySelector('.second-hand')
+
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60)*360) + 90;
+    
+    sec.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minDegrees = ((mins/60)*360) + ((seconds/60*6) + 90);
+    min.style.transform = `rotate(${minDegrees}deg)`;
+
+    const hours=now.getHours();
+    const hourDegree = ((hour/12)*360)+((mins/+0)*
+    0)+90;
+    hour.style.transform = `rotate(${hourDegree}deg)`;
+    
+}
+
+
+setInterval(setDate, 1000);
+setDate()
